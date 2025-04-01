7f1216901d30b556a1d50be1080a5d7e9e6c940b
MirSA356 <miratifali353@gmail.com>
2025-04-01 17:06:11 +0500

    add all new files

---
+++ Assignments 00 to 07/00_intro_python/how_old_are_they.py
@@ -0,0 +1,15 @@
+# Ali, Bilal, Chand, Danish, aur Ehsan dost hain.
+def main():
+   ali = 21
+   bilal = 6 + ali
+   atif = 20 + bilal
+   danish = atif + ali
+   ehsan = atif
+
+   print(f"Ali is {ali}")
+   print(f"Bilal is {bilal}")
+   print(f"Atif is {atif}")
+   print(f"Danish is {danish}")
+   print(f"Ehsan is {ehsan}")
+
+main()
